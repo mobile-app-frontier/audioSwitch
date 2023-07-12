93940fcb7319765ad8180dbc051248a1bd22c33c
DHL <ekaeae@gmail.com>
2023-07-12 13:12:42 +0900

    [feat] audio scanner 기능 추가

--- audioswitch/src/main/java/com/kt/audioswitch/AudioSwitch.kt
+++ audioswitch/src/main/java/com/kt/audioswitch/AudioSwitch.kt
@@ -1,23 +1,21 @@
 package com.kt.audioswitch
 
-import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.media.AudioManager
-import android.media.AudioManager.OnAudioFocusChangeListener
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
 import androidx.annotation.VisibleForTesting
 import com.kt.audioswitch.AudioDevice.BluetoothHeadset
 import com.kt.audioswitch.AudioDevice.Earpiece
 import com.kt.audioswitch.AudioDevice.Speakerphone
 import com.kt.audioswitch.AudioDevice.WiredHeadset
-import com.kt.audioswitch.AudioSwitch.State.ACTIVATED
-import com.kt.audioswitch.AudioSwitch.State.STARTED
-import com.kt.audioswitch.AudioSwitch.State.STOPPED
 import com.kt.audioswitch.android.Logger
 import com.kt.audioswitch.android.ProductionLogger
-import com.kt.audioswitch.bluetooth.BluetoothHeadsetConnectionListener
-import com.kt.audioswitch.bluetooth.BluetoothHeadsetManager
-import com.kt.audioswitch.wired.WiredDeviceConnectionListener
-import com.kt.audioswitch.wired.WiredHeadsetReceiver
+import com.kt.audioswitch.scanners.AudioDeviceScanner
+import com.kt.audioswitch.scanners.Scanner
+import java.util.*
 
 private const val TAG = "AudioSwitch"
 
@@ -31,57 +29,9 @@ private const val TAG = "AudioSwitch"
  * default.
  * @property selectedAudioDevice Retrieves the selected [AudioDevice] from [AudioSwitch.selectDevice].
  * @property availableAudioDevices Retrieves the current list of available [AudioDevice]s.
-**/
-class AudioSwitch {
-
-    private var logger: Logger = ProductionLogger()
-    private val audioDeviceManager: AudioDeviceManager
-    private val wiredHeadsetReceiver: WiredHeadsetReceiver
-    internal var audioDeviceChangeListener: AudioDeviceChangeListener? = null
-    private var selectedDevice: AudioDevice? = null
-    private var userSelectedDevice: AudioDevice? = null
-    private var wiredHeadsetAvailable = false
-    private val mutableAudioDevices = ArrayList<AudioDevice>()
-    private var bluetoothHeadsetManager: BluetoothHeadsetManager? = null
-    private val preferredDeviceList: List<Class<out AudioDevice>>
-
-    internal var state: State = STOPPED
-    internal enum class State {
-        STARTED, ACTIVATED, STOPPED
-    }
-
-    internal val bluetoothDeviceConnectionListener = object : BluetoothHeadsetConnectionListener {
-        override fun onBluetoothHeadsetStateChanged(headsetName: String?) {
-            enumerateDevices(headsetName)
-        }
-
-        override fun onBluetoothHeadsetActivationError() {
-            if (userSelectedDevice is BluetoothHeadset) userSelectedDevice = null
-            enumerateDevices()
-        }
-    }
-
-    internal val wiredDeviceConnectionListener = object : WiredDeviceConnectionListener {
-        override fun onDeviceConnected() {
-            wiredHeadsetAvailable = true
-            enumerateDevices()
-        }
-
-        override fun onDeviceDisconnected() {
-            wiredHeadsetAvailable = false
-            enumerateDevices()
-        }
-    }
-
-    var loggingEnabled: Boolean
-        get() = logger.loggingEnabled
-
-        set(value) {
-            logger.loggingEnabled = value
-        }
-    val selectedAudioDevice: AudioDevice? get() = selectedDevice
-    val availableAudioDevices: List<AudioDevice> = mutableAudioDevices
-
+ **/
+@RequiresApi(Build.VERSION_CODES.M)
+class AudioSwitch : AbstractAudioSwitch {
     /**
      * Constructs a new AudioSwitch instance.
      * - [context] - An Android Context.
@@ -102,277 +52,84 @@ class AudioSwitch {
     constructor(
         context: Context,
         loggingEnabled: Boolean = false,
-        audioFocusChangeListener: OnAudioFocusChangeListener = OnAudioFocusChangeListener {},
+        audioFocusChangeListener: AudioManager.OnAudioFocusChangeListener = AudioManager.OnAudioFocusChangeListener {},
         preferredDeviceList: List<Class<out AudioDevice>> = defaultPreferredDeviceList
-    ) : this(context.applicationContext, ProductionLogger(loggingEnabled), audioFocusChangeListener,
-            preferredDeviceList)
+    ) : this(
+        context, audioFocusChangeListener, ProductionLogger(loggingEnabled), preferredDeviceList
+    )
 
+    /**
+     * Constructs a new AudioSwitch instance.
+     * - [context] - An Android Context.
+     * - [loggingEnabled] - Toggle whether logging is enabled. This argument is false by default.
+     * - [audioFocusChangeListener] - A listener that is invoked when the system audio focus is updated.
+     * Note that updates are only sent to the listener after [activate] has been called.
+     * - [preferredDeviceList] - The order in which [AudioSwitch] automatically selects and activates
+     * an [AudioDevice]. This parameter is ignored if the [selectedAudioDevice] is not `null`.
+     * The default preferred [AudioDevice] order is the following:
+     * [BluetoothHeadset], [WiredHeadset], [Earpiece], [Speakerphone]
+     * . The [preferredDeviceList] is added to the front of the default list. For example, if [preferredDeviceList]
+     * is [Speakerphone] and [BluetoothHeadset], then the new preferred audio
+     * device list will be:
+     * [Speakerphone], [BluetoothHeadset], [WiredHeadset], [Earpiece].
+     * An [IllegalArgumentException] is thrown if the [preferredDeviceList] contains duplicate [AudioDevice] elements.
+     */
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     internal constructor(
         context: Context,
+        audioFocusChangeListener: AudioManager.OnAudioFocusChangeListener,
         logger: Logger,
-        audioFocusChangeListener: OnAudioFocusChangeListener,
         preferredDeviceList: List<Class<out AudioDevice>>,
-        audioDeviceManager: AudioDeviceManager = AudioDeviceManager(context,
+        audioManager: AudioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager,
+        audioDeviceManager: AudioDeviceManager = AudioDeviceManager(
+            context,
             logger,
-            context.getSystemService(Context.AUDIO_SERVICE) as AudioManager,
-            audioFocusChangeListener = audioFocusChangeListener),
-        wiredHeadsetReceiver: WiredHeadsetReceiver = WiredHeadsetReceiver(context, logger),
-        headsetManager: BluetoothHeadsetManager? = BluetoothHeadsetManager.newInstance(context,
-            logger,
-            BluetoothAdapter.getDefaultAdapter(),
-            audioDeviceManager)
-    ) {
-        this.logger = logger
-        this.audioDeviceManager = audioDeviceManager
-        this.wiredHeadsetReceiver = wiredHeadsetReceiver
-        this.bluetoothHeadsetManager = headsetManager
-        this.preferredDeviceList = getPreferredDeviceList(preferredDeviceList)
-        logger.d(TAG, "AudioSwitch($VERSION)")
-        logger.d(TAG, "Preferred device list = ${this.preferredDeviceList.map { it.simpleName }}")
-    }
-
-    private fun getPreferredDeviceList(preferredDeviceList: List<Class<out AudioDevice>>):
-            List<Class<out AudioDevice>> {
-        require(hasNoDuplicates(preferredDeviceList))
-
-        return if (preferredDeviceList.isEmpty() || preferredDeviceList == defaultPreferredDeviceList) {
-            defaultPreferredDeviceList
-        } else {
-            val result = defaultPreferredDeviceList.toMutableList()
-            result.removeAll(preferredDeviceList)
-            preferredDeviceList.forEachIndexed { index, device ->
-                result.add(index, device)
-            }
-            result
-        }
-    }
-
-    /**
-     * Starts listening for audio device changes and calls the [listener] upon each change.
-     * **Note:** When audio device listening is no longer needed, [AudioSwitch.stop] should be
-     * called in order to prevent a memory leak.
-     */
-    fun start(listener: AudioDeviceChangeListener) {
-        audioDeviceChangeListener = listener
-        when (state) {
-            STOPPED -> {
-                state = STARTED
-                enumerateDevices()
-                bluetoothHeadsetManager?.start(bluetoothDeviceConnectionListener)
-                wiredHeadsetReceiver.start(wiredDeviceConnectionListener)
-            }
-            else -> {
-                logger.d(TAG, "Redundant start() invocation while already in the started or activated state")
-            }
-        }
-    }
-
-    /**
-     * Stops listening for audio device changes if [AudioSwitch.start] has already been
-     * invoked. [AudioSwitch.deactivate] will also get called if a device has been activated
-     * with [AudioSwitch.activate].
-     */
-    fun stop() {
-        when (state) {
-            ACTIVATED -> {
-                deactivate()
-                closeListeners()
-            }
-            STARTED -> {
-                closeListeners()
-            }
-            STOPPED -> {
-                logger.d(TAG, "Redundant stop() invocation while already in the stopped state")
-            }
-        }
-    }
-
-    /**
-     * Performs audio routing and unmuting on the selected device from
-     * [AudioSwitch.selectDevice]. Audio focus is also acquired for the client application.
-     * **Note:** [AudioSwitch.deactivate] should be invoked to restore the prior audio
-     * state.
-     */
-    fun activate() {
-        when (state) {
-            STARTED -> {
-                state = ACTIVATED
-                audioDeviceManager.cacheAudioState()
-
-                // Always set mute to false for WebRTC
-                audioDeviceManager.mute(false)
-                audioDeviceManager.setAudioFocus()
-                selectedDevice?.let { activate(it) }
-            }
-            ACTIVATED -> selectedDevice?.let { activate(it) }
-            STOPPED -> throw IllegalStateException()
-        }
-    }
-
-    /**
-     * Restores the audio state prior to calling [AudioSwitch.activate] and removes
-     * audio focus from the client application.
-     */
-    fun deactivate() {
-        when (state) {
-            ACTIVATED -> {
-                state = STARTED
-                bluetoothHeadsetManager?.deactivate()
-
-                // Restore stored audio state
-                audioDeviceManager.restoreAudioState()
-            }
-            STARTED, STOPPED -> {
-            }
-        }
-    }
-
-    /**
-     * Selects the desired [audioDevice]. If the provided [AudioDevice] is not
-     * available, no changes are made. If the provided device is null, one is chosen based on the
-     * specified preferred device list or the following default list:
-     * [BluetoothHeadset], [WiredHeadset], [Earpiece], [Speakerphone].
-     */
-    fun selectDevice(audioDevice: AudioDevice?) {
-        if (selectedDevice != audioDevice) {
-            logger.d(TAG, "Selected AudioDevice = $audioDevice")
-            userSelectedDevice = audioDevice
-            enumerateDevices()
-        }
-    }
-
-    private fun hasNoDuplicates(list: List<Class<out AudioDevice>>) =
-        list.groupingBy { it }.eachCount().filter { it.value > 1 }.isEmpty()
-
-    private fun activate(audioDevice: AudioDevice) {
-        when (audioDevice) {
-            is BluetoothHeadset -> {
-                audioDeviceManager.enableSpeakerphone(false)
-                bluetoothHeadsetManager?.activate()
-            }
-            is Earpiece, is WiredHeadset -> {
-                audioDeviceManager.enableSpeakerphone(false)
-                bluetoothHeadsetManager?.deactivate()
-            }
-            is Speakerphone -> {
-                audioDeviceManager.enableSpeakerphone(true)
-                bluetoothHeadsetManager?.deactivate()
-            }
-        }
-    }
-
-    internal data class AudioDeviceState(
-        val audioDeviceList: List<AudioDevice>,
-        val selectedAudioDevice: AudioDevice?
+            audioManager,
+            audioFocusChangeListener = audioFocusChangeListener
+        ),
+        handler: Handler = Handler(Looper.getMainLooper()),
+        scanner: Scanner = AudioDeviceScanner(audioManager, handler),
+    ) : super(
+        context = context,
+        audioFocusChangeListener = audioFocusChangeListener,
+        scanner = scanner,
+        logger = logger,
+        preferredDeviceList = preferredDeviceList,
+        audioDeviceManager = audioDeviceManager,
     )
 
-    private fun enumerateDevices(bluetoothHeadsetName: String? = null) {
-        // save off the old state and 'semi'-deep copy the list of audio devices
-        val oldAudioDeviceState = AudioDeviceState(mutableAudioDevices.map { it }, selectedDevice)
-        // update audio device list and selected device
-        addAvailableAudioDevices(bluetoothHeadsetName)
-
-        if (!userSelectedDevicePresent(mutableAudioDevices)) {
-            userSelectedDevice = null
+    override fun onDeviceDisconnected(audioDevice: AudioDevice) {
+        this.logger.d(TAG_AUDIO_SWITCH, "onDeviceDisconnected($audioDevice)")
+        var wasChanged = this.availableUniqueAudioDevices.remove(audioDevice)
+        if (this.userSelectedAudioDevice == audioDevice) {
+            this.userSelectedAudioDevice = null
         }
 
-        // Select the audio device
-        logger.d(TAG, "Current user selected AudioDevice = $userSelectedDevice")
-        selectedDevice = if (userSelectedDevice != null) {
-            userSelectedDevice
-        } else if (mutableAudioDevices.size > 0) {
-            val firstAudioDevice = mutableAudioDevices[0]
-            /*
-             * If there was an error starting bluetooth sco, then the selected AudioDevice should
-             * be the next valid device in the list.
-             */
-            if (firstAudioDevice is BluetoothHeadset &&
-                    bluetoothHeadsetManager?.hasActivationError() == true) {
-                mutableAudioDevices[1]
-            } else {
-                firstAudioDevice
-            }
-        } else {
-            null
+        if (audioDevice is WiredHeadset && this.audioDeviceManager.hasEarpiece()) {
+            wasChanged = wasChanged || this.availableUniqueAudioDevices.add(Earpiece())
         }
-
-        // Activate the device if in the active state
-        if (state == ACTIVATED) {
-            activate()
-        }
-        // trigger audio device change listener if there has been a change
-        val newAudioDeviceState = AudioDeviceState(mutableAudioDevices, selectedDevice)
-        if (newAudioDeviceState != oldAudioDeviceState) {
-            audioDeviceChangeListener?.invoke(mutableAudioDevices, selectedDevice)
-        }
+        this.selectAudioDevice(wasChanged)
     }
 
-    private fun addAvailableAudioDevices(bluetoothHeadsetName: String?) {
-        mutableAudioDevices.clear()
-        preferredDeviceList.forEach { audioDevice ->
-            when (audioDevice) {
-                BluetoothHeadset::class.java -> {
-                /*
-                 * Since the there is a delay between receiving the ACTION_ACL_CONNECTED event and receiving
-                 * the name of the connected device from querying the BluetoothHeadset proxy class, the
-                 * headset name received from the ACTION_ACL_CONNECTED intent needs to be passed into this
-                 * function.
-                 */
-                bluetoothHeadsetManager?.getHeadset(bluetoothHeadsetName)?.let {
-                        mutableAudioDevices.add(it)
-                    }
-                }
-                WiredHeadset::class.java -> {
-                    if (wiredHeadsetAvailable) {
-                        mutableAudioDevices.add(WiredHeadset())
-                    }
-                }
-                Earpiece::class.java -> {
-                    if (audioDeviceManager.hasEarpiece() && !wiredHeadsetAvailable) {
-                        mutableAudioDevices.add(Earpiece())
-                    }
-                }
-                Speakerphone::class.java -> {
-                    if (audioDeviceManager.hasSpeakerphone()) {
-                        mutableAudioDevices.add(Speakerphone())
-                    }
-                }
+    override fun onActivate(audioDevice: AudioDevice) {
+        this.logger.d(TAG_AUDIO_SWITCH, "onActivate($audioDevice)")
+        when (audioDevice) {
+            is BluetoothHeadset -> {
+                this.audioDeviceManager.enableSpeakerphone(false)
+                this.audioDeviceManager.enableBluetoothSco(true)
+            }
+            is Earpiece, is WiredHeadset -> {
+                this.audioDeviceManager.enableSpeakerphone(false)
+                this.audioDeviceManager.enableBluetoothSco(false)
+            }
+            is Speakerphone -> {
+                this.audioDeviceManager.enableBluetoothSco(false)
+                this.audioDeviceManager.enableSpeakerphone(true)
             }
         }
-
-        logger.d(TAG, "Available AudioDevice list updated: $availableAudioDevices")
     }
 
-    private fun userSelectedDevicePresent(audioDevices: List<AudioDevice>) =
-            userSelectedDevice?.let { selectedDevice ->
-                if (selectedDevice is BluetoothHeadset) {
-                    // Match any bluetooth headset as a new one may have been connected
-                    audioDevices.find { it is BluetoothHeadset }?.let { newHeadset ->
-                        userSelectedDevice = newHeadset
-                        true
-                    } ?: false
-                } else {
-                    audioDevices.contains(selectedDevice)
-                }
-            } ?: false
-
-    private fun closeListeners() {
-        state = STOPPED
-        bluetoothHeadsetManager?.stop()
-        wiredHeadsetReceiver.stop()
-        audioDeviceChangeListener = null
-    }
-
-    companion object {
-        /**
-         * The version of the AudioSwitch library.
-         */
-        const val VERSION = BuildConfig.VERSION_NAME
-
-        private val defaultPreferredDeviceList by lazy {
-            listOf(BluetoothHeadset::class.java,
-                    WiredHeadset::class.java, Earpiece::class.java, Speakerphone::class.java)
-        }
+    override fun onDeactivate() {
+        this.logger.d(TAG_AUDIO_SWITCH, "onDeactivate")
     }
 }
